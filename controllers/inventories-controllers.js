565e8479c60baf619496f9cf4e5ead28450378e5
Matthew Rios <riverground@outlook.com>
2024-03-28 14:25:52 -0400

    feat(neil-25-26): :sparkles: created get all items and get one item for the inventories api.

--- controllers/inventories-controllers.js
+++ controllers/inventories-controllers.js
@@ -1,30 +1,61 @@
-const knex = require('knex')(require('../knexfile'));
+const knex = require("knex")(require("../knexfile"));
+
+const getAll = async (_req, res) => {
+  try {
+    const data = await knex("inventories");
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(400).send(`Error retrieving Inventories: ${err}`);
+  }
+};
+
+const findOne = async (req, res) => {
+  try {
+    const inventoriesFound = await knex("inventories").where({
+      id: req.params.id,
+    });
+
+    if (inventoriesFound.length === 0) {
+      return res.status(404).json({
+        message: `Inventory with ID ${req.params.id} not found`,
+      });
+    }
+
+    const inventoriesData = inventoriesFound[0];
+    res.json(inventoriesData);
+  } catch (error) {
+    res.status(500).json({
+      message: `Unable to retrieve inventories data for inventory with ID ${req.params.id}`,
+    });
+  }
+};
 
 const deleteOne = async (req, res) => {
-    try {
-        const id = req.params.id;
-        const deletedInventoryItem = await knex('inventories')
-            .where({ id: id })
-            .del();
+  try {
+    const id = req.params.id;
+    const deletedInventoryItem = await knex("inventories")
+      .where({ id: id })
+      .del();
 
-        if (deletedInventoryItem  === 0) {
-            return res.status(404).send({
-                message: `Inventory Item with ID ${id} not found.`
-            });
-        }
-
-        res.status(204).send({ 
-            message: 'Inventory Item deleted successfully.' 
-        });
-    } catch (error) {
-        res.status(500).send({
-            message: 'Error deleting the Inventory Item.',
-            error: error.message
-        });
+    if (deletedInventoryItem === 0) {
+      return res.status(404).send({
+        message: `Inventory Item with ID ${id} not found.`,
+      });
     }
+
+    res.status(204).send({
+      message: "Inventory Item deleted successfully.",
+    });
+  } catch (error) {
+    res.status(500).send({
+      message: "Error deleting the Inventory Item.",
+      error: error.message,
+    });
+  }
 };
 
 module.exports = {
- 
-    deleteOne
-  }
+  getAll,
+  findOne,
+  deleteOne,
+};
--- routes/inventories-routes.js
+++ routes/inventories-routes.js
@@ -1,6 +1,10 @@
 const router = require('express').Router();
 const inventoriesController = require('../controllers/inventories-controllers.js');
 
+router.get('/', inventoriesController.getAll);
+
+router.get('/:id', inventoriesController.findOne);
+
 router.delete('/inventories/:id', inventoriesController.deleteOne);
 
 module.exports = router;
